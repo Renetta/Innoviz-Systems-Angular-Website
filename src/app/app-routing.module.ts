836c1eab73f66d06f22edcbec7933c6490d816d7
renetta <renetta92@gmail.com>
2022-01-31 00:50:52 +0300

    Home page, components created . dropdown menu fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/home"},
+  { path: "home", component: HomeComponent},
+  { path: "our-company", component: AboutComponent},
+  { path: "services", component: ServicesComponent},
+  { path: "careers", component: CareersComponent},
+  { path: "contact", component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
