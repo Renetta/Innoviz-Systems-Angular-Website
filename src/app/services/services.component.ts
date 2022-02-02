bf86950d677592de8f9e0205fbe497bc1c96442b
renetta <renetta92@gmail.com>
2022-02-02 18:08:21 +0300

    service page updated

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private httpService: HttpClient) { }
+  services : any = [];
+  serviceImages : any = [];
+  serviceOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
   ngOnInit(): void {
+    this.httpService.get<any>('./assets/innovizDatas.json').subscribe(
+      (res) => {
+        this.services = res.ServiceComponents;
+        this.serviceImages = res.Services;
+      }
+    )
   }
 
 }
